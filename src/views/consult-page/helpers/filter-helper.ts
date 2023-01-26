19207218d54e5b49711fb401cd48bc2731b352eb
Vergiliogu <vergiliopoleza@gmail.com>
2023-01-26 19:30:15 -0300

    refactor: convert search filter to lowercase before test

--- src/views/consult-page/helpers/filter-helper.ts
+++ src/views/consult-page/helpers/filter-helper.ts
@@ -11,8 +11,8 @@ export const applyFilter: ApplyFilterProps = (data, filters) => {
 
         for (let i = 0, range = filters.length; i < range; i++) {
             const filterField = filters[i].field;
-            const filterSearch = filters[i].search;
-            if (!new RegExp(filterSearch).test(dataObj[filterField])) {
+            const filterSearch = filters[i].search.toLowerCase();
+            if (!new RegExp(filterSearch).test(dataObj[filterField].toLowerCase())) {
                 return false;
             }
         }
