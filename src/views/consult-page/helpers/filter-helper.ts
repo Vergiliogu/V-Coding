32614c8b42bcb7eb067ac70617ed11cc7b78e678
Vergiliogu <vergiliopoleza@gmail.com>
2023-01-26 19:24:10 -0300

    feat: create applyFilter helper

---
+++ src/views/consult-page/helpers/filter-helper.ts
@@ -0,0 +1,24 @@
+import { FilterDataProps } from "../consult-page.types"
+import { MockDataProps } from "../../../mock-data/mock-data.types"
+
+type ApplyFilterProps = (
+    data: MockDataProps[],
+    filters: FilterDataProps[]
+) => MockDataProps[];
+
+export const applyFilter: ApplyFilterProps = (data, filters) => {
+    const filteredData = data.filter(dataObj => {
+
+        for (let i = 0, range = filters.length; i < range; i++) {
+            const filterField = filters[i].field;
+            const filterSearch = filters[i].search;
+            if (!new RegExp(filterSearch).test(dataObj[filterField])) {
+                return false;
+            }
+        }
+
+        return true;
+    })
+
+    return filteredData;
+}
