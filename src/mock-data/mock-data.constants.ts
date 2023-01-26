50e7003b745fc12176f54cf908869bee8fba0bac
Vergiliogu <vergiliopoleza@gmail.com>
2023-01-26 19:14:21 -0300

    feat: add mock data

---
+++ src/mock-data/mock-data.constants.ts
@@ -0,0 +1,8 @@
+export const mockDataKeys = [
+    "email",
+    "first_name",
+    "last_name",
+    "primary_group",
+    "phone_number",
+    "hours_studied"
+]
---
+++ src/mock-data/mock-data.types.ts
@@ -0,0 +1,10 @@
+export type MockDataProps = {
+    email: string;
+    first_name: string;
+    last_name: string;
+    primary_group: string;
+    phone_number: string;
+    hours_studied: string;
+}
+
+export type MockDataKeysProps = keyof MockDataProps
