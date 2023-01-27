01f5c49c1793a6676c7381b329b292154a12c8bf
Vergiliogu <vergiliopoleza@gmail.com>
2023-01-26 22:17:07 -0300

    fix: filter helper throws error when data is empty

--- src/views/consult-page/helpers/filter-helper.ts
+++ src/views/consult-page/helpers/filter-helper.ts
@@ -12,7 +12,7 @@ export const applyFilter: ApplyFilterProps = (data, filters) => {
         for (let i = 0, range = filters.length; i < range; i++) {
             const filterField = filters[i].field;
             const filterSearch = filters[i].search.toLowerCase();
-            if (!new RegExp(filterSearch).test(dataObj[filterField].toLowerCase())) {
+            if (!new RegExp(filterSearch).test((dataObj[filterField] || "").toLowerCase())) {
                 return false;
             }
         }
